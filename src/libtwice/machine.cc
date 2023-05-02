3e1cec95383e6166dec187a8881600e272bd1979
Azusa <azusachan1111@gmail.com>
2023-05-02 19:49:14 +1000

    memory size definitions

--- src/libtwice/machine.cc
+++ src/libtwice/machine.cc
@@ -6,11 +6,11 @@ using namespace twice;
 
 Machine::Machine(Config& config)
 	: config(config),
-	  arm7_bios(config.data_dir + "bios7.bin", NDS_BIOS7_SIZE,
+	  arm7_bios(config.data_dir + "bios7.bin", ARM7_BIOS_SIZE,
 			  FileMap::MAP_EXACT_SIZE),
-	  arm9_bios(config.data_dir + "bios9.bin", NDS_BIOS9_SIZE,
+	  arm9_bios(config.data_dir + "bios9.bin", ARM9_BIOS_SIZE,
 			  FileMap::MAP_EXACT_SIZE),
-	  firmware(config.data_dir + "firmware.bin", NDS_FIRMWARE_SIZE,
+	  firmware(config.data_dir + "firmware.bin", FIRMWARE_SIZE,
 			  FileMap::MAP_EXACT_SIZE)
 {
 }
@@ -20,7 +20,7 @@ Machine::~Machine() = default;
 void
 Machine::load_cartridge(const std::string& pathname)
 {
-	cartridge = { pathname, NDS_MAX_CART_SIZE, FileMap::MAP_MAX_SIZE };
+	cartridge = { pathname, MAX_CART_SIZE, FileMap::MAP_MAX_SIZE };
 }
 
 void
--- src/libtwice/nds.h
+++ src/libtwice/nds.h
@@ -5,12 +5,31 @@
 
 namespace twice {
 
-constexpr size_t NDS_BIOS7_SIZE = 16_KiB;
-constexpr size_t NDS_BIOS9_SIZE = 4_KiB;
-constexpr size_t NDS_FIRMWARE_SIZE = 256_KiB;
-constexpr size_t NDS_MAX_CART_SIZE = 512_MiB;
+enum MemorySizes : u32 {
+	ITCM_SIZE = 32_KiB,
+	ITCM_MASK = 32_KiB - 1,
+	DTCM_SIZE = 16_KiB,
+	DTCM_MASK = 16_KiB - 1,
+	MAIN_RAM_SIZE = 4_MiB,
+	MAIN_RAM_MASK = 4_MiB - 1,
+	SHARED_WRAM_SIZE = 32_KiB,
+	SHARED_WRAM_MASK = 32_KiB - 1,
+	PALETTE_SIZE = 2_KiB,
+	PALETTE_MASK = 2_KiB - 1,
+	OAM_SIZE = 2_KiB,
+	OAM_MASK = 2_KiB - 1,
+	ARM9_BIOS_SIZE = 4_KiB,
+	ARM9_BIOS_MASK = 4_KiB - 1,
+	ARM7_BIOS_SIZE = 16_KiB,
+	ARM7_BIOS_MASK = 16_KiB - 1,
+	ARM7_WRAM_SIZE = 64_KiB,
+	ARM7_WRAM_MASK = 64_KiB - 1,
+	FIRMWARE_SIZE = 256_KiB,
+	MAX_CART_SIZE = 512_MiB,
+};
 
 struct NDS {
+
 	NDS(u8 *, u8 *, u8 *, u8 *, size_t);
 	~NDS();
 
