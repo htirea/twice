51f35907d9e676c4b682147614919971963b9481
Azusa <azusachan1111@gmail.com>
2023-05-18 14:19:08 +1000

    writeback pc tests for arm load / store

--- test/test-roms/twice-suite/arm9/writeback.s
+++ test/test-roms/twice-suite/arm9/writeback.s
@@ -1,22 +1,112 @@
 @ vim:ft=armv5
 
+.macro check_load
+	orr r0, #1
+	orr r0, #2
+	orr r0, #4
+	orr r0, #8
+	cmp r0, #15
+	bne fail
+.endm
+
 .text
 .global test_writeback
 
 test_writeback:
 	push {r4-r11, lr}
 
-	@ test 1: ldr writeback
+	@ test 1: ldr pc writeback
 	mov r11, #1
-
 	mov r0, #0
 	@ldr r1, [pc], #4
 	.long 0xE49F1004
+	check_load
+
+	@ test 2: ldrb pc writeback
+	mov r11, #2
+	mov r0, #0
+	@ldrb r1, [pc], #4
+	.long 0xE4DF1004
+	check_load
+
+	@ test 3: ldrh pc writeback
+	mov r11, #3
+	mov r0, #0
+	@ldrh r1, [pc], #4
+	.long 0xE0DF10B4
+	check_load
+
+	@ test 4: ldrsh pc writeback
+	mov r11, #4
+	mov r0, #0
+	@ldrsh r1, [pc], #4
+	.long 0xE0DF10F4
+	check_load
+
+	@ test 5: ldrsb pc writeback
+	mov r11, #5
+	mov r0, #0
+	@ldrsb r1, [pc], #4
+	.long 0xE0DF10D4
+	check_load
+
+	@ test 6: ldrd pc writeback
+	mov r11, #6
+	mov r0, #0
+	@ldrd r2, [pc], #4
+	.long 0xE0CF20D4
+	check_load
+
+	@ test 7: str pc writeback
+	mov r11, #7
+	mov r0, #0
+	ldr r1, =0xE3800004
+	@str r1, [pc, #4]!
+	.long 0xE5AF1004
 	orr r0, #1
 	orr r0, #2
-	orr r0, #4
+	nop
 	orr r0, #8
+	cmp r0, #15
+	bne fail
 
+	@ test 8: strb pc writeback
+	mov r11, #8
+	mov r0, #0
+	mov r1, #04
+	@strb r1, [pc, #4]!
+	.long 0xE5EF1004
+	orr r0, #1
+	orr r0, #2
+	.long 0xE3800000
+	orr r0, #8
+	cmp r0, #15
+	bne fail
+
+	@ test 9: strh pc writeback
+	mov r11, #9
+	mov r0, #0
+	mov r1, #4
+	@ strh r1, [pc, #4]!
+	.long 0xE1EF10B4
+	orr r0, #1
+	orr r0, #2
+	.long 0xE3800000
+	orr r0, #8
+	cmp r0, #15
+	bne fail
+
+	@ test 0xA: strd pc writeback
+	mov r11, #0xA
+	mov r0, #0
+	ldr r2, =0xE3800004
+	ldr r3, =0xE3800008
+	@ strd r2, [pc, #4]!
+	.long 0xE1EF20F4
+	orr r0, #1
+	orr r0, #2
+	nop
+	nop
 	cmp r0, #15
 	bne fail
 
