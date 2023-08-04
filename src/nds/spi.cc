8f1bc84ba12b91fb46c908a7c656788e0df650ea
Azusa <azusachan1111@gmail.com>
2023-08-04 21:03:33 +1000

    fix spicnt write

--- src/nds/spi.cc
+++ src/nds/spi.cc
@@ -20,7 +20,7 @@ void
 spicnt_write(nds_ctx *nds, u16 value)
 {
 	u16 old = nds->spicnt;
-	nds->spicnt = (nds->spicnt & ~0xCF03) & (value & 0xCF03);
+	nds->spicnt = (nds->spicnt & ~0xCF03) | (value & 0xCF03);
 	if (!(old & BIT(15)) && (value & BIT(15))) {
 		nds->spicnt &= ~BIT(11);
 	}
