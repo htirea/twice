7f829c88aca7d39279efcb10d79a51c365331029
Azusa <azusachan1111@gmail.com>
2023-08-04 23:50:47 +1000

    remove incorrect spi chip select behaviour

--- src/nds/spi.cc
+++ src/nds/spi.cc
@@ -21,9 +21,6 @@ spicnt_write(nds_ctx *nds, u16 value)
 {
 	u16 old = nds->spicnt;
 	nds->spicnt = (nds->spicnt & ~0xCF03) | (value & 0xCF03);
-	if (!(old & BIT(15)) && (value & BIT(15))) {
-		nds->spicnt &= ~BIT(11);
-	}
 }
 
 } // namespace twice
