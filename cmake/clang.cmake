7e3f6dc156e83fcab40f61a2a4cf0a8aa19245d4
Azusa <azusachan1111@gmail.com>
2023-12-23 13:52:47 +1100

    use submodules for dependencies

--- cmake/clang.cmake
+++ cmake/clang.cmake
@@ -11,6 +11,7 @@ set(CMAKE_CXX_FLAGS_INIT " \
 -Wimplicit-fallthrough")
 
 set(CMAKE_CXX_COMPILER clang++)
+set(CMAKE_C_COMPILER clang)
 set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld")
 set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=lld")
 set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld")
--- cmake/gcc.cmake
+++ cmake/gcc.cmake
@@ -19,6 +19,7 @@ set(CMAKE_CXX_FLAGS_INIT " \
 -Wimplicit-fallthrough=5")
 
 set(CMAKE_CXX_COMPILER g++)
+set(CMAKE_C_COMPILER gcc)
 set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=gold")
 set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=gold")
 set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=gold")
