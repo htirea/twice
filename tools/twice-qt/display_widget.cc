d758f877d4b8957f70a2056907f8fb4a4a884f4c
Azusa <azusachan1111@gmail.com>
2024-01-09 02:59:50 +1100

    fix texture clamp

--- tools/twice-qt/display_widget.cc
+++ tools/twice-qt/display_widget.cc
@@ -102,13 +102,14 @@ DisplayWidget::initializeGL()
 	glGenTextures(1, &texture);
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, texture);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	float borderColor[] = { 0.0f, 0.0f, 0.0f, 0.0f };
+	glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, NDS_FB_W, NDS_FB_H, 0, GL_RGBA,
 			GL_UNSIGNED_BYTE, tbuffer->get_read_buffer().data());
-	glGenerateMipmap(GL_TEXTURE_2D);
 	glUseProgram(shader_program);
 	GLuint proj_mtx_loc = glGetUniformLocation(shader_program, "proj_mtx");
 	glUniformMatrix4fv(proj_mtx_loc, 1, GL_FALSE, proj_mtx);
